36e2b9cb7c00f9cde3537f3c58eb7b26a8d75c2b
VYO <yaravirus86@gmail.com>
2024-01-29 12:12:45 -0500

    Initial Commit

---
+++ MinecraftClone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MinecraftClone
+//
+//  Created by Yaroslav Valchyshen on 1/29/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
