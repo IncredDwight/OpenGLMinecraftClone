a35f959e8c0ec6bfe307c8a5aeb9fb6e683b9b2a
VYO <yaravirus86@gmail.com>
2024-02-08 10:24:48 -0500

    Project creation

--- MinecraftClone/main.cpp
+++ MinecraftClone/main.cpp
@@ -1,14 +1,39 @@
-//
-//  main.cpp
-//  MinecraftClone
-//
-//  Created by Yaroslav Valchyshen on 1/29/24.
-//
+#include <GL/glew.h>
 
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
