ed2f7cd675e24cb5dfc91bbdf3f2d147641913d5
VYO <yaravirus86@gmail.com>
2025-12-28 08:43:47 -0500

    Position change

--- main.cpp
+++ main.cpp
@@ -81,7 +81,7 @@ int main(void)
     }
     
     glm::mat4 model = glm::mat4(1.0f);
-    glm::vec3 position = glm::vec3(0, 2, 1.5f);
+    glm::vec3 position = glm::vec3(10, 30, 10);
     Camera camera(position, 45.0f, shader);
     
     shader.SetUniformMat4f("u_model", model);
@@ -93,35 +93,7 @@ int main(void)
     
     BatchRenderer::Init();
     std::vector<Vertex> verticies = Cube::GetVerticies(snowGrassBlock);
-    
-    /*for (int x = 0; x < 50; x++) {
-            for (int z = 0; z < 50; z++) {
-                CubeTextureCoord* texture = nullptr;
-                
-                float noise = SimplexNoise::noise(x * scale + offset.x, z * scale + offset.y);
-                
-                if(noise >= 0.5f){
-                    texture = &snowGrassBlock;
-                }
-                else if(noise >= -0.2f)
-                {
-                    texture = &grassBlock;
-                }
-                else if(noise >= -0.5f)
-                {
-                    texture = &dirtBlock;
-                }
-                else
-                    texture = &stoneBlock;
-                cube.push_back({glm::vec3(x, (int)(noise * 10), z), *texture});
-                for (int y = -12; y < (int)(noise * 10); y++) {
-                    cube.push_back({glm::vec3(x, y, z), (noise - y <= 5) ? dirtBlock : stoneBlock});
-                }
-                
-            }
-        }
-    */
-    
+
     std::vector<Vertex> verticies0 = Cube::GetVerticies(grassBlock);
     std::vector<Vertex> verticies1 = Cube::GetVerticies(snowGrassBlock);
     std::vector<Vertex> dirtVerticies = Cube::GetVerticies(dirtBlock);
@@ -136,9 +108,7 @@ int main(void)
         
         glClear(GL_COLOR_BUFFER_BIT);
         shader.SetUniformMat4f("u_model", model);
-        /*cube.Update(shader);
-        vertexArray.LoadLayouts();
-        cube1.Update(shader);*/
+
         vertexArray.LoadLayouts();
         
         cameraMovement.MoveRight();
@@ -151,12 +121,7 @@ int main(void)
         vertexArray.LoadLayouts();
         
         vertexArray.LoadLayouts();
-        //BatchRenderer::Update();
-        
-        //scale *= 1.0005;
-        
-        //offset = glm::vec2(offset.x + 0.0005f, offset.y + 0.0005f);
-        
+
         if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
             offset = glm::vec2(offset.x + 0.05f, offset.y);
         if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
